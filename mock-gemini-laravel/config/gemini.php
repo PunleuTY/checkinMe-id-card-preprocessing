2e3b08c29600eb897a7794e8725838d5aba5702f
PUNLEU <punleutyy@gmail.com>
2026-05-28 09:56:11 +0700

    feat: add gemini config file

---
+++ mock-gemini-laravel/config/gemini.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'base_url' => env('GEMINI_SERVICE_BASE_URL', 'http://localhost:8000'),
+    'timeout' => env('GEMINI_SERVICE_TIMEOUT', 30),
+    'model' => env('GEMINI_SERVICE_MODEL', 'gemini-2.5-flash'),
+    'endpoints' => [
+        'scan_nid' => env('GEMINI_SERVICE_ENDPOINTS_SCAN_NID', '/gemini-ocr/upload/processed'),
+    ]
+];
+
